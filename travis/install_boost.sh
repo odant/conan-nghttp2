2522fb2d688f7b87c420ad770d06e29e60248903
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2018-08-08 19:30:09 +0300

    Squashed 'src/' content from commit 64f0372d
    git-subtree-dir: src
    git-subtree-split: 64f0372de5d4633cf21b13949ddb1bc1a7cc646a

---
+++ travis/install_boost.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "Installing Boost ${CI_BOOST_VERSION}"
+
+_startup_dir=`pwd`
+
+export CI_BOOST_ROOT=${HOME}/boost
+
+# Download Boost
+mkdir -p ${HOME}/boost_source
+CI_BOOST_VERSION_=`echo -n ${CI_BOOST_VERSION} | sed 's/\./_/g'`
+CI_BOOST_URL="https://dl.bintray.com/boostorg/release/${CI_BOOST_VERSION}/source/boost_${CI_BOOST_VERSION_}.tar.gz"
+travis_retry wget --no-check-certificate --quiet -O - ${CI_BOOST_URL} | tar --strip-components=1 -xz -C ${HOME}/boost_source
+
+# Build Boost
+mkdir -p ${HOME}/boost/include
+cd ${HOME}/boost_source
+
+
+if [[ $CXX == g++* ]]; then
+    CI_B2_TOOLSET="gcc"
+fi
+if [[ "$CXX" == clang* ]]; then
+    CI_B2_TOOLSET="clang"
+fi
+
+echo "using ${CI_B2_TOOLSET} : : ${CXX} ;" > tools/build/src/user-config.jam;
+
+./bootstrap.sh --with-toolset=${CI_B2_TOOLSET}
+
+CI_B2_OPTIONS="--with-atomic --with-chrono --with-date_time --with-regex --with-serialization --with-system --with-thread --build-type=minimal --stagedir=${CI_BOOST_ROOT}"
+CI_B2_PROPERTIES="threading=multi link=static variant=${CI_BUILD_TYPE,,} cflags=-fPIC cxxflags=-fPIC" toolset=${CI_B2_TOOLSET}
+./b2 -d2 -q ${CI_B2_OPTIONS} ${CI_B2_PROPERTIES} stage
+mv ${HOME}/boost_source/boost ${CI_BOOST_ROOT}/include/
+
+cd ${_startup_dir}
+unset _startup_dir
+
+echo "Done installing Boost. \${CI_BOOST_ROOT}=${CI_BOOST_ROOT}"
+
